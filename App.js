0817e19b437458eae46c6d9c8cfb1efc7d2763f5
Tiago Amado Durante <tiagodurante@icloud.com>
2019-08-08 20:49:58 -0300

    primeiro hello world

--- App.js
+++ App.js
@@ -1,6 +1,26 @@
+/* eslint-disable prettier/prettier */
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 export default function App() {
-  return <Text>Hello World</Text>;
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello World</Text>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    display: 'flex',
+    backgroundColor: '#7159c1',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontWeight: 'bold',
+    color: '#FFF',
+    fontSize: 20,
+  },
+});
